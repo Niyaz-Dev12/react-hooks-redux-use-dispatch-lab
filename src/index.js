134ce9d454ff3b82dfd320334f59e7d78127090c
Niyaz <niyaz.irfan119@gmail.com>
2021-08-29 22:00:23 +0300

    commit tree 4dc30e2b38f384d78c8058f066c162b7a736c9b7

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import restaurantsReducer from "./features/restaurant/restaurantsSlice";
+import restaurantsReducer from './features/restaurant/restaurantsSlice';
 
-const store = createStore(restaurantsReducer);
+const store = createStore(
+  restaurantsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App store={store} />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
